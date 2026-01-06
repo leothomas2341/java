d27eebfa1550079fe631f348626a6db4f9be16a9
Léo Thomas <leothomas@Mac.lan>
2026-01-06 11:15:19 +0100

    TP2 Exercice 1 (méthode 1)

---
+++ src/Tp2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Tp2 {
+    private static Scanner scanner;
+    public static void main (String[] args){
+        scanner = new Scanner(System.in);
+
+        exercice1();
+
+        scanner.close();
+    }
+    public static void exercice1(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Saisir un nombre : ");
+        int n = scanner.nextInt();
+        // méthode 1
+        int f0 = 0;
+        int f1 = 1;
+
+        for (int i = 2; i<=n; i++){
+            int next = f0 + f1;
+            System.out.println(next);
+            f0 = f1;
+            f1 = next;
+        }
+
+        System.out.println("fibo à F" + n + " = " + f1);
+
+
+    }
+}
