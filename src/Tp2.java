428dad0f3b32067e5e5db0e4c6e1cb851012fca9
Léo Thomas <leothomas@Mac.lan>
2026-01-06 15:43:19 +0100

    TP2 Exercice 2

--- src/Tp2.java
+++ src/Tp2.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 
 public class Tp2 {
+
     private static Scanner scanner;
     public static void main (String[] args){
-        scanner = new Scanner(System.in);
+//        scanner = new Scanner(System.in);
+//
+//        exercice1();
+//
+//        scanner.close();
 
-        exercice1();
+        int[] tab = { 4, 6, 4, 2, 10, 2, 4, 8, 10, 1, 4, 9, 8, 9, 4, 1, 8, 2, 9 };
+        int[] tab1 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-        scanner.close();
+        int[] tab2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        int[] tab3 = { 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+        int[] tab4 = { 2023, 2016, 2014, 2032, 2011, 2022, 2014, 1992, 2014, 1998, 2020, 2028, 2011, 2009, 2017, 2001,
+                2028, 2014, 2025 };
+
+        triComptage(tab);
     }
     public static void exercice1(){
 
@@ -45,5 +57,50 @@ public class Tp2 {
         }
 
         System.out.println("fibo à f" + n + " = " + fibo[n]);
+    }
+    public static void triComptage(int[] tab){
+        printTab(tab);
+
+        //Valeurs min max
+        int min = tab[0];
+        int max = tab[0];
+
+        for(int i=1; i<tab.length; i++){
+            if (tab[i]<min){
+                min = tab[i];
+            }
+            if (tab[i]>max){
+                max=tab[i];
+            }
+        }
+
+        //Tableau de comptage
+        int plage = max-min+1;
+        int[] tabComptage=new int[plage];
+
+        //Compter les occurences
+        for (int i = 0; i<tab.length; i++){
+            tabComptage[tab[i] - min]++;
+        }
+
+        //Tableau trié
+        int index=0;
+        for(int i=0; i<tabComptage.length; i++){
+            while(tabComptage[i]>0){
+                tab[index]=i+min;
+                index++;
+                tabComptage[i]--;
+            }
+        }
+
+        System.out.println("Tableau trié :");
+        printTab(tab);
+    }
+
+    private static void printTab(int[] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(tab[i] + " ");
+        }
+        System.out.println();
     }
 }
